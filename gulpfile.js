91f3e77de56c532a7097e0fd1952b29bdd331287
DESKTOP-H3T9CKD\WangWH <b10234033.ntust@gmail.com>
2022-11-28 17:38:08 +0800

    首次編譯成功

--- gulpfile.js
+++ gulpfile.js
@@ -1,146 +1,3 @@
-const {
-    src,
-    dest,
-    series,
-    parallel,
-    watch
-} = require('gulp'); //匯入會使用的特定 gulp api。需要用到時，就可以直接呼叫
-
-// rename 
-const rename = require('gulp-rename');
-//js uglify
-const uglify = require('gulp-uglify');
-// css minify
-const cleanCSS = require('gulp-clean-css');
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
-const fileinclude = require('gulp-file-include');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-
-//1.html
-async function includeHTML() {
-    return src('*.html')
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(dest('./dist'));
-}
-
-exports.includeHTML =  includeHTML;
-
-const cleanCSS = require('gulp-clean-css');
-
-
-gulp.task('minify_css', function () {
-  //do
-  return gulp.src('css/*.css') //來源
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(gulp.dest('dest/css')) //目的地
-})
-
-
-// 2.sass
-
-
-function sassstyle(){ 
-    return src('sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(dest('dist/css')) 
-}
-
-exports.style = sassstyle
-
-//3. js
-
-function  Jsminify(){
-    return src('js/*.js')
-    // .pipe(uglify())
-    // .pipe(rename({
-    //     extname: '.min.js' 
-    // }))
-    .pipe(dest('dist/js'))
-}
-
-exports.uglify = Jsminify;
-
-const babel = require('gulp-babel');
-
-function babel5() {
-    return src('js/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
-        .pipe(dest('dist/js'));
-}
-
-exports.js_update = babel5;
-
-
-//4.搬家
-function img_move(){
-    return src(['images/*.*' , 'images/**/*.*']).pipe(dest('dist/images'))
- }
-
- const imagemin = require('gulp-imagemin');
-
-
- // mini images
- function min_images(){
-     return src(['images/*.*' , 'images/**/*.*'])
-     .pipe(imagemin([
-         imagemin.mozjpeg({quality: 70, progressive: true}) // 壓縮品質      quality越低 -> 壓縮越大 -> 品質越差 
-     ]))
-     .pipe(dest('dist/images'))
- }
-
- exports.images_online = min_images;
-
- function php(){
-    return src(['php/*.*' , 'php/**/*.*']).pipe(dest('dist/php'))
- }
-
-//clear old file
-const clean = require('gulp-clean');
-
-function clear() {
-  return src('dist' ,{ read: false ,allowEmpty: true })//不去讀檔案結構，增加刪除效率  / allowEmpty : 允許刪除空的檔案
-  .pipe(clean({force: true})); //強制刪除檔案 
-}
-
-exports.cls = clear;
-
-
- // 瀏覽器同步
- function browser(done) {
-    browserSync.init({
-        server: {
-            baseDir: "./dist",
-            index: "index.html"
-        },
-        port: 3000
-    });
-    watch(['*.html' , 'layout/*.html'] , includeHTML).on('change' ,reload)  
-    watch(['sass/*.scss' , 'sass/**/*.scss'] , sassstyle).on('change' ,reload)
-    watch(['images/*.*' , 'images/**/*.*'] , img_move).on('change' , reload)
-    watch('js/*.js' ,Jsminify).on('change' ,reload)
-    watch('php/*.php' ,php).on('change' ,reload)
-    done();
-}
-
-//執行
-
-exports.default = series(parallel(includeHTML , sassstyle ,img_move , Jsminify, php) ,browser)
-// exports.online =series(clear , parallel(includeHTML , sassstyle,min_images , babel5, php))
-
-//------------
-
 // const {
 //     src,
 //     dest,
@@ -163,7 +20,7 @@ exports.default = series(parallel(includeHTML , sassstyle ,img_move , Jsminify, 
 
 // //1.html
 // function includeHTML() {
-//     return src('*.html')
+//     return src('**.html')
 //         .pipe(fileinclude({
 //             prefix: '@@',
 //             basepath: '@file'
@@ -178,11 +35,11 @@ exports.default = series(parallel(includeHTML , sassstyle ,img_move , Jsminify, 
 
 
 // function sassstyle(){ 
-//     return src('sass/*.scss')
+//     return src('sass/**.scss')
 //     .pipe(sourcemaps.init())
 //     .pipe(sass.sync().on('error', sass.logError))
 //     .pipe(sourcemaps.write())
-//     .pipe(dest('dist/css')) 
+//     .pipe(dest('./dist/css')) 
 // }
 
 // exports.style = sassstyle
@@ -190,12 +47,12 @@ exports.default = series(parallel(includeHTML , sassstyle ,img_move , Jsminify, 
 // //3. js
 
 // function  Jsminify(){
-//     return src('js/*.js')
+//     return src('js/**.js')
 //     .pipe(uglify())
 //     // .pipe(rename({
 //     //     extname: '.min.js' 
 //     // }))
-//     .pipe(dest('dist/js'))
+//     .pipe(dest('./dist/js'))
 // }
 
 // exports.uglify = Jsminify;
@@ -213,7 +70,7 @@ exports.default = series(parallel(includeHTML , sassstyle ,img_move , Jsminify, 
 //             baseDir: "./dist",
 //             index: "index.html"
 //         },
-//         port: 3000
+//         port: 5555
 //     });
 //     watch(['*.html' , 'layout/*.html'] , includeHTML).on('change' ,reload)  
 //     watch(['sass/*.scss' , 'sass/**/*.scss'] , sassstyle).on('change' ,reload)
@@ -224,3 +81,21 @@ exports.default = series(parallel(includeHTML , sassstyle ,img_move , Jsminify, 
 
 // //執行
 // exports.default = series(parallel(includeHTML , sassstyle ,img_move , Jsminify) ,browser)
+
+//--------------------------------------------------------
+
+//可成功編譯
+
+var gulp = require('gulp');
+var fileinclude  = require('gulp-file-include');
+
+gulp.task('fileinclude', function() {
+    gulp.src('**.html')
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: '@file'
+        }))
+    .pipe(gulp.dest('./dist'));
+});
+
+//---------------------------------------------------------
